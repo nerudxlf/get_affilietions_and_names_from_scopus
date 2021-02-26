4a362255c177b5313cceeca3fa29b80e854c6847
nerudxlf <nerudxlf@gmail.com>
2021-02-26 13:28:23 +0600

    fix bugs with name

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
-from icecream import ic
 
 
 def get_name(split_list: list) -> str:
+    if len(split_list[1]) > 6:
+        return split_list[0]
     return split_list[0] + " " + split_list[1]
 
 
@@ -11,6 +12,8 @@ def get_university(split_list: list) -> str:
     dep = "Department"
     lab = "Laboratory"
     if split_list[2].find(devel) == -1 and split_list[2].find(dep) == -1 and split_list[2].find(lab) == -1:
+        if len(split_list[1]) > 6:
+            return split_list[1]
         return split_list[2]
     return split_list[3]
 
@@ -43,7 +46,7 @@ def main():
             university_list.append(get_university(elem_split))
             country_list.append(get_country(elem_split))
         except IndexError:
-            university_list.append("-")
-            country_list.append("-")
+            university_list.append("Not")
+            country_list.append("Not")
     result_df = pd.DataFrame({'Author': name_list, 'University': university_list, 'Country': country_list})
     result_df.to_excel("result.xlsx", index=False)
